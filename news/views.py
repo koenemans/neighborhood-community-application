49b12456171ef7765406fdaff58539b653e6971e
Koenraad <koenemans@proton.me>
2025-04-25 14:09:47 +0200

    Add basic news routing to index and detail templates

--- news/views.py
+++ news/views.py
@@ -1,4 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the news index.")
+    latest_posts_list = Post.objects.order_by('-created_at')[:5]
+    context = { "latest_posts_list": latest_posts_list }
+    return render(request, 'news/index.html', context)
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    return render(request, 'news/detail.html', { 'post': post })
