2ef1c849eaa8fa034f0af12dfac2e94f9d45f613
Koenraad <koenemans@proton.me>
2025-04-25 14:46:22 +0200

    Move apps to generic views

--- activities/urls.py
+++ activities/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = 'activities'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:activity_id>/', views.detail, name='detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 ]
