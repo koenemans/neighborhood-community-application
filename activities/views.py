51af2556261fde01cdb9131e1bc62b3c491299ff
Koenraad <koenemans@proton.me>
2025-04-27 10:29:07 +0200

    Cleanup

--- activities/views.py
+++ activities/views.py
@@ -1,14 +1,14 @@
-from django.views import generic
+from django.views.generic import ListView, DetailView
 
 from .models import Activity
 
-class IndexView(generic.ListView):
+class IndexView(ListView):
     template_name = 'activities/index.html'
     context_object_name = 'upcoming_activity_list'
 
     def get_queryset(self):
         return Activity.objects.order_by('-start')[:5]
     
-class DetailView(generic.DetailView):
+class DetailView(DetailView):
     model = Activity
     template_name = 'activities/detail.html'
