c8a76d59ac2335d5f2ef57f6ac753f2eb4dd2b2a
Koenraad <koenemans@proton.me>
2025-04-27 10:29:07 +0200

    Cleanup

--- news/views.py
+++ news/views.py
@@ -1,8 +1,8 @@
-from django.views import generic
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
-class IndexView(generic.ListView):
+class IndexView(ListView):
     template_name = 'news/index.html'
     context_object_name = 'latest_posts_list'
 
@@ -10,6 +10,6 @@ class IndexView(generic.ListView):
         """Return the last five published posts."""
         return Post.objects.order_by('-created_at')[:5]
     
-class DetailView(generic.DetailView):
+class DetailView(DetailView):
     model = Post
     template_name = 'news/detail.html'
