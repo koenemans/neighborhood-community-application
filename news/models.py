1621fe0d1ab614727433f6cca059b0b9db6161e9
Koenraad <koenemans@proton.me>
2025-04-28 09:02:57 +0200

    Add committee as author to news and activity models

--- news/models.py
+++ news/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import Group
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    committee = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='news')
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
