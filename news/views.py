0bc65383923cc21dfd5490cb0e389768e1861807
Koenraad <koenemans@proton.me>
2025-05-05 13:00:35 +0200

    Add filter by committee on news and activities archive

--- news/views.py
+++ news/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView, base, TemplateView
 
+from committees.models import Committee
 from .models import Post
 
 class HomePageView(TemplateView):
@@ -18,19 +19,33 @@ class DetailView(DetailView):
 
 class NewsArchiveView(base.TemplateView):
     template_name = 'news/archive.html'
+    context_object_name = 'grouped_news'
+
+    def get_queryset(self):
+        # Filter posts by committee if a query parameter is provided
+        queryset = Post.objects.all().order_by('-created_at')
+        committee_slug = self.request.GET.get('committee')
+        if committee_slug:
+            queryset = queryset.filter(committee__slug=committee_slug)
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        all_news = Post.objects.all().order_by('-created_at')
+        queryset = self.get_queryset()
 
-        # Group posts by year, then by month, then by committee
+        # Group posts by year and month
         grouped_news = {}
-        for post in all_news:
+        for post in queryset:
             year = post.created_at.year
-            month = post.created_at.strftime('%B')  # Get month name
-            committee = post.committee if post.committee else "No Committee"
+            month = post.created_at.strftime('%B')
+            grouped_news.setdefault(year, {}).setdefault(month, []).append(post)
 
-            grouped_news.setdefault(year, {}).setdefault(month, {}).setdefault(committee, []).append(post)
-
+        # Add grouped news and committees to the context
         context['grouped_news'] = grouped_news
+        context['all_committees'] = Committee.objects.all()
+        committee_slug = self.request.GET.get('committee')
+        if committee_slug:
+            context['filtered_committee'] = Committee.objects.filter(slug=committee_slug).first()
+        else:
+            context['filtered_committee'] = None
         return context
