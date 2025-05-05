ba6c9a46a65cccf96d4da451218f681e4fb618c5
Koenraad <koenemans@proton.me>
2025-05-05 12:50:32 +0200

    Adding template routing and functionalities

--- news/urls.py
+++ news/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'news'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('archive/', views.NewsArchiveView.as_view(), name='news_archive'),
+    path('archive/', views.NewsArchiveView.as_view(), name='archive'),
     path('<slug:slug>/', views.DetailView.as_view(), name='detail')
 ]
