056ee41bc85c3190cfd224ef8e0bb380ab82c1b7
Koenraad <koenemans@proton.me>
2025-05-10 09:51:57 +0200

    Add attachment field to news model

--- news/models.py
+++ news/models.py
@@ -13,7 +13,8 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
-    poster = models.ImageField(upload_to='posters/', blank=True, null=True)
+    poster = models.ImageField(upload_to='news/posters/%Y/%m/%d/', blank=True, null=True)
+    attachement = models.FileField(upload_to='news/attachments/%Y/%m/%d/', blank=True, null=True)
     committee = models.ForeignKey(Committee, on_delete=models.CASCADE, related_name='news')
     created_at = models.DateTimeField(default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
@@ -25,6 +26,12 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse('news:detail', args=[self.slug])
+    
+    def has_poster(self):
+        return bool(self.poster)
+    
+    def has_attachment(self):
+        return bool(self.attachement)
 
     def __str__(self):
         return self.title
