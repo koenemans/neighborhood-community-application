1ff7bec91c78c2b56eaccc775fa44bef8d102371
Koenraad <koenemans@proton.me>
2025-05-13 20:22:13 +0200

    Initial setup of i18n with nl locales

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'nl'
 
 TIME_ZONE = 'UTC'
 
--- news/models.py
+++ news/models.py
@@ -3,21 +3,22 @@ from django.utils import timezone
 from django.urls import reverse
 from django.utils.text import slugify
 from committees.models import Committee
+from django.utils.translation import gettext_lazy as _
 
 class Post(models.Model):
     class Meta:
-        verbose_name = 'Post'
-        verbose_name_plural = 'News'
+        verbose_name = _('Post')
+        verbose_name_plural = _('News')
         ordering = ['-created_at']
 
-    title = models.CharField(max_length=200)
+    title = models.CharField(_('title'), max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
-    content = models.TextField()
+    content = models.TextField(_('content'))
     poster = models.ImageField(upload_to='news/posters/%Y/%m/%d/', blank=True, null=True)
-    attachment = models.FileField(upload_to='news/attachments/%Y/%m/%d/', blank=True, null=True)
-    committee = models.ForeignKey(Committee, on_delete=models.CASCADE, related_name='news')
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
+    attachment = models.FileField(_('attachment'), upload_to='news/attachments/%Y/%m/%d/', blank=True, null=True)
+    committee = models.ForeignKey(Committee, on_delete=models.CASCADE, related_name='news', verbose_name=_('committee'))
+    created_at = models.DateTimeField(_('created at'), default=timezone.now)
+    updated_at = models.DateTimeField(_('updated at'), auto_now=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -26,12 +27,6 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return reverse('news:detail', args=[self.slug])
-    
-    def has_poster(self):
-        return bool(self.poster)
-    
-    def has_attachment(self):
-        return bool(self.attachement)
 
     def __str__(self):
         return self.title
